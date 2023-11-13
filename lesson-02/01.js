21758ec84b55828ed3a4ceb1b409dd0e83da8aad
KristinaFinck <kristana.finck83@gmail.com>
2023-11-13 12:13:13 +0200

    delete ':'

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,7 +1,7 @@
 let number1 = Math.floor(Math.random() * 100)
 if (number1 % 2 === 0) {
-    console.log(number1 + ":" + "Это чётное число")
+    console.log(number1 + ":Это чётное число")
     }else{
-    console.log(number1 + ":" + "Это нечётное число")
+    console.log(number1 + ":lsЭто нечётное число")
 }
 
